ea8f4b7c651e1ddefc4939eb846394b7247742ee
Utkarsh Mishra <ilyaasmis17@gmail.com>
2024-11-07 12:56:06 +0530

    modified main.cpp file

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,33 @@
-#include <iostream>
+#include <iostream>  
+
+using namespace std; 
+
+
+void printMessage() {
+    cout << "This is my first CMake program!" << endl;
+}
 
 int main() {
-    std::cout << "This is my first CMake project" << std::endl;
+
+    printMessage();
+    cout << "Behold my first C++ based calculator!" << endl;
+
+    int num1, num2;
+    
+    cout << "Enter the first number: ";
+    cin >> num1;
+    cout << "Enter the second number: ";
+    cin >> num2;
+
+    int sum = num1 + num2;
+    int difference = num1 - num2;
+    int product = num1 * num2;
+    float quotient = static_cast<float>(num1) / num2;
+
+    cout << "Sum: " << sum << endl;
+    cout << "Difference: " << difference << endl;
+    cout << "Product: " << product << endl;
+    cout << "Quotient: " << quotient << endl;
+
     return 0;
 }
